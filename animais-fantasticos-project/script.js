02eb4ebfca5564af14ea36f8bc223a3e86a006b0
AelcioJozias <aelciojoziasputzel18@gmail.com>
2024-03-11 20:40:41 -0300

    exemplo de Navegação por tabs com animação

--- animais-fantasticos-project/script.js
+++ animais-fantasticos-project/script.js
@@ -1,1 +1,26 @@
-    
+function iniciarNavegacaoPorFoto() {
+    /* esse 'js' estamos usando ele como um root, aplicando
+    * todo o resto do código apenas se a class 'js' existir no elemento 'html', o que significa que o js
+    * da página está ativo
+    *  */
+    document.documentElement.classList.add('js')
+
+    const tabMenu = document.querySelectorAll('.js section.js-tabmenu li')
+    const tabContent = document.querySelectorAll('.js .js-tabcontent section')
+
+    function mostrarConteudo(index) {
+        tabContent.forEach(e => e.classList.remove('ativo'))
+        tabContent[index].classList.add('ativo')
+    }
+
+    tabMenu.forEach((elemement, index) => {
+        elemement.addEventListener('click', e => {
+            mostrarConteudo(index)
+        })
+    })
+}
+
+iniciarNavegacaoPorFoto();
+
+
+
