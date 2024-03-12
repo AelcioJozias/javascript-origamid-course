6a337b60bd0d08bbf08cef4a63261adaca13c13f
Aelcio Jozias <aelciojoziasp@gmail.com>
2024-03-11 22:35:18 -0300

    exemplo de como fazer um Accordion List

--- animais-fantasticos-project/script.js
+++ animais-fantasticos-project/script.js
@@ -1,4 +1,6 @@
 function iniciarNavegacaoPorFoto() {
+
+
     /* esse 'js' estamos usando ele como um root, aplicando
     * todo o resto do código apenas se a class 'js' existir no elemento 'html', o que significa que o js
     * da página está ativo
@@ -8,19 +10,46 @@ function iniciarNavegacaoPorFoto() {
     const tabMenu = document.querySelectorAll('.js section.js-tabmenu li')
     const tabContent = document.querySelectorAll('.js .js-tabcontent section')
 
-    function mostrarConteudo(index) {
-        tabContent.forEach(e => e.classList.remove('ativo'))
-        tabContent[index].classList.add('ativo')
+    if (tabMenu.length && tabContent.length) {
+        function mostrarConteudo(index) {
+            tabContent.forEach(e => e.classList.remove('ativo'))
+            tabContent[index].classList.add('ativo')
+        }
+
+        tabMenu.forEach((elemement, index) => {
+            elemement.addEventListener('click', e => {
+                mostrarConteudo(index)
+            })
+        })
     }
 
-    tabMenu.forEach((elemement, index) => {
-        elemement.addEventListener('click', e => {
-            mostrarConteudo(index)
-        })
-    })
 }
 
 iniciarNavegacaoPorFoto();
+
+function initAccordion() {
+    const dtList = document.querySelectorAll('.js-accordion dt')
+    const ddList = document.querySelectorAll('.js-accordion dd')
+    if (dtList.length && ddList.length) {
+        dtList[0].nextElementSibling.classList.add('ativo')
+        dtList[0].classList.add('ativo')
+
+        dtList.forEach(dt => dt.addEventListener('click', mostrarConteudo))
+
+        function mostrarConteudo() {
+            this.classList.toggle(`ativo`)
+            this.nextElementSibling.classList.toggle('ativo')
+        }
+    }
+
+}
+
+initAccordion();
+
+
+
+
+
 
 
 
