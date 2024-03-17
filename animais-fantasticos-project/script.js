c685e2d760611c96211800b8702d2367e6279a00
Aelcio Jozias <aelciojoziasp@gmail.com>
2024-03-17 14:53:28 -0300

    levando o script de add js class de um método e levando para o arquivo html principal

--- animais-fantasticos-project/script.js
+++ animais-fantasticos-project/script.js
@@ -1,11 +1,5 @@
 function iniciarNavegacaoPorFoto() {
 
-    /* esse 'js' estamos usando ele como um root, aplicando
-    * todo o resto do código apenas se a class 'js' existir no elemento 'html', o que significa que o js
-    * da página está ativo
-    *  */
-    document.documentElement.classList.add('js')
-
     const tabMenu = document.querySelectorAll('.js section.js-tabmenu li')
     const tabContent = document.querySelectorAll('.js .js-tabcontent section')
 
@@ -62,6 +56,10 @@ function initSmoothScroll() {
 }
 
 initSmoothScroll()
+
+
+
+
 
 
 
